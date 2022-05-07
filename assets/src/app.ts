f8397cd7e849eb00f5aea5f4353ad5e2291c3c61
Tyler Holewinski <tsh6656@rit.edu>
2022-05-07 13:01:30 -0400

    cleanup hanging modules

--- assets/src/app.ts
+++ assets/src/app.ts
@@ -2,20 +2,19 @@ import 'phoenix_html'; // Include phoenix_html to handle method=PUT/DELETE in fo
 import { Socket } from 'phoenix'; // Establish Phoenix Socket and LiveView configuration.
 import { LiveSocket } from 'phoenix_live_view';
 
-import '../css/app.css'; // tell esbuild to bundle css as well
-
 let csrfToken = document
-    .querySelector("meta[name='csrf-token']")
-    .getAttribute('content');
+  .querySelector("meta[name='csrf-token']")
+  .getAttribute('content');
+
 let liveSocket = new LiveSocket('/live', Socket, {
-    params: { _csrf_token: csrfToken },
+  params: { _csrf_token: csrfToken },
 });
 
 // I swear I like TS, even when it pulls this shennaigans
 declare global {
-    interface Window {
-        liveSocket: LiveSocket;
-    }
+  interface Window {
+    liveSocket: LiveSocket;
+  }
 }
 
 liveSocket.connect();
--- config/config.exs
+++ config/config.exs
@@ -12,7 +12,7 @@ config :esbuild,
   version: "0.14.0",
   default: [
     args:
-      ~w(ts/app.ts ts/pages/index.ts --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+      ~w(src/app.ts src/pages/index.ts --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
