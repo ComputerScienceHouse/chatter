835abe2b77c2b132bd9693a766682a127a835da4
Tyler Holewinski <tsh6656@rit.edu>
2022-05-29 18:46:50 -0400

    plugs/assign_git_rev: indicate dev builds

--- lib/chatter_web/plugs/assign_git_rev.ex
+++ lib/chatter_web/plugs/assign_git_rev.ex
@@ -4,7 +4,7 @@ defmodule ChatterWeb.Plugs.AssignGitRev do
   def init(default), do: default
 
   def call(conn, _default) do
-    {git_rev, 0} = System.cmd("git", ["rev-parse", "--short", "HEAD"])
+    {git_rev, 0} = if System.get_env("MIX_ENV") == "prod", do: System.cmd("git", ["rev-parse", "--short", "HEAD"]), else: {"master", 0}
     assign(conn, :git_rev, git_rev |> String.trim("\n"))
   end
 end
